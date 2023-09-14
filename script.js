bcae2bb4bffcecd9d01488628a918cfc134c27c2
mdonmezer <muhammetdonmezer1@gmail.com>
2023-09-14 15:52:51 +0300

    Proje Tamamlandı.

--- script.js
+++ script.js
@@ -1,24 +1,133 @@
-const form = document.querySelector('form');
-const input = document.querySelector //Notumuzu ekleyebilmek için bu işlemi yaptık.
-('#txtNotName');
-const btnDeleteAll = document.querySelector // Hepsini sil butonumuzu aktif ettik.
-('btnDeleteAll');
-const Notlist = document.querySelector //Not listemizi aktif ettik.
-('#Not-list');
-//Fonksiyon kullanıp tek bir yerden kontrol ettik.
+// Todo Eleman Ekleme
+
+// Eleman Seçimi
+
+const form = document.querySelector("form");
+const input = document.querySelector("#txtTaskName");
+const btnAddNewTask = document.querySelector("#btnAddNewTask");
+const btnDeleteAll = document.querySelector("#btnDeleteAll");
+const taskList = document.querySelector("#task-list");
+let todos;
+
+
+// load items
+loadItems();
+
+
 eventListeners();
 
-function eventListeners(){
-    form.addEventListener('submit',addNewItem)
-    
-} 
+function eventListeners() {
+    // Not Kaydet
+    form.addEventListener("submit", addNewItem);
+    // Not Sil
+    taskList.addEventListener("click", deleteItem);
+    // Hepsini Sil
+    btnDeleteAll.addEventListener("click", deleteAllItems);
 
-function addNewItem(e){
-    if(input.value ===''){
-        alert('Lütfen Bir Not Giriniz!') //Boş bir metin eklenmesi durumunda uyarı vermesini sağladık.
+}
+
+function loadItems() {
+    todos = getItemsFromLS();
+    todos.forEach(function (item) {
+        createItem(item);
+    })
+
+}
+// Notları Localden alıyoruz
+
+function getItemsFromLS(){
+    if(localStorage.getItem("todos") === null){
+        todos = [];
     }
-    
-    
-    
+    else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+
+}
+// Notları Locale kaydediyoruz.
+
+function setItemToLS(newTodo){
+    todos = getItemsFromLS();
+    todos.push(newTodo);
+    localStorage.setItem("todos",JSON.stringify(todos));
+
+}
+
+function createItem(newTodo) {
+    // li oluşturma
+
+    const li = document.createElement("li");
+    li.className = "list-group-item list-group-item-secondary";
+    li.appendChild(document.createTextNode(newTodo));
+
+    // a oluşturma
+
+    const a = document.createElement("a");
+    a.classList = "delete-item float-right";
+    a.setAttribute("href", "#");
+    a.innerHTML = '<i class="fas fa-times"></i>';
+
+    li.appendChild(a);
+
+    taskList.appendChild(li);
+}
+
+
+function addNewItem(e) {
+    if (input.value === '') {
+        alert("Lütfen Not Girin!");
+    }
+
+    // Not Oluşturma
+
+    createItem(input.value);
+
+    setItemToLS(input.value);
+
+
+
+
+
+    input.value = "";
+
     e.preventDefault();
+
+}
+
+// Eleman Sildik
+
+function deleteItem(e) {
+
+    if (e.target.className === "fas fa-times") {
+        if (confirm("Silmek istediğinize emin misiniz?")) {
+            e.target.parentElement.parentElement.remove();
+            deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
+        }
+    }
+    e.preventDefault();
+
+}
+function deleteTodoFromStorage(deletetodo){
+    let todos = getItemsFromLS();
+
+    todos.forEach(function(todo,index){
+        if(todo === deletetodo){
+            todos.splice(index,1);
+        }
+    })
+    localStorage.setItem("todos",JSON.stringify(todos));
+}
+
+// Tüm Elemanları Sildik
+
+function deleteAllItems(e) {
+
+    if (confirm("Tüm elemanları silmek istediğinize emin misiniz?")) {
+        
+        while(taskList.firstChild){
+            taskList.removeChild(taskList.firstChild);
+        }
+        localStorage.clear();
+    }
 }
